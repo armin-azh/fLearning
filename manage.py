07aefbe5ad7a818d404e9d5cd8504f4936e86630
armin azhdehnia <arminmk18@gmail.com>
2021-09-20 23:07:09 -0400

    add parser to the main

---
+++ manage.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import os
+import argparse
+from argparse import Namespace
+
+
+def main(arguments: Namespace) -> None:
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    main(args)
---
+++ settings.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import os
+from pathlib import Path
+
+BASE_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
