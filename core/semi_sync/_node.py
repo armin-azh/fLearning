2cf1e67b5400545b8468553844ab8be558f96608
armin azhdehnia <arminmk18@gmail.com>
2021-10-31 01:53:13 -0400

    aaa

--- core/semi_sync/_node.py
+++ core/semi_sync/_node.py
@@ -16,6 +16,9 @@ from core.utils import receive, send, fed_avg
 
 
 class ServerNode:
+    SocketConnections = None  # nd array
+    Accumulator = []
+
     def __init__(self, hostname: Tuple[str, int],
                  connections: List[Tuple[str, int]],
                  model,
@@ -49,7 +52,7 @@ class ServerNode:
         self._socket.bind(self._hostname)
         self._socket.listen()
 
-        self._receive_models = []
+        self._receive_models = None
         self._n_com = 0
 
         # start connection and build graph process
@@ -61,12 +64,18 @@ class ServerNode:
         # end connection and build graph process
 
     def _mk_accept(self):
+        _idx = 0
         while self._n_connected < self._total_connections:
             print(f"[{self._node_name}] [{self._n_connected + 1}|{self._total_connections}] Connected, "
                   f"listening on {self._hostname[0]}:{self._hostname[1]}")
             conn, add = self._socket.accept()
             self._conn.append((conn, add))
             self._n_connected += 1
+
+            self._glob_node_lock.acquire()
+            ServerNode.SocketConnections[_idx] = 1
+            self._glob_node_lock.release()
+            _idx += 1
 
         print(f"[{self._node_name}] Successfully all node are connected.")
 
@@ -87,5 +96,96 @@ class ServerNode:
             except ConnectionRefusedError:
                 pass
 
+    def send_all(self, conn, **kwargs):
+        send(conn=conn, net=self._model)
+
+    def receive_all(self, conn, _idx, add, barrier, to_master, **kwargs):
+        r_model = receive(conn=conn)
+        print(f"[{self._node_name}] received model {_idx + 1} on {add}")
+        self._lock.acquire()
+        ServerNode.Accumulator.append((r_model, conn))
+        self._n_com += 1
+        self._lock.release()
+
+    def _sub_server(self, conn, n_round):
+
+        for _idx in range(n_round):
+            pass
+
+    def exec_(self, start_barrier: Barrier, opt, criterion, opt_conf):
+        print(f'[{self._node_name}] now is running')
+        opt = opt(self._model.parameters(), **opt_conf)
+
+        start_barrier.wait()
+        while True:
+            # start, check all connections are gone
+            self._glob_node_lock.acquire()
+            if np.all(ServerNode.SocketConnections == 0):
+                self._glob_node_lock.release()
+                break
+            self._glob_node_lock.release()
+            # end, check all connections are gone
 
 
+class ClientNode:
+    def __init__(self, hostname: Tuple[str, int],
+                 connection: Tuple[str, int],
+                 model,
+                 glob_lock: Lock,
+                 host_idx: int,
+                 save_path: Path):
+        self._node_name = f"Client_{host_idx}"
+        self._save_path = save_path.joinpath(self._node_name)
+        self._save_path.mkdir(parents=True, exist_ok=True)
+        self._save_weights = self._save_path.joinpath("weight")
+        self._save_weights.mkdir(parents=True, exist_ok=True)
+
+        self._mark = None
+
+        # start make identification
+        self._hostname = hostname
+        # end make identification
+
+        self._connection = connection
+        self._glob_node_lock = glob_lock
+
+        self._lock = threading.Lock()
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        self._socket_conn = None  # online connections to send
+        self._conn = None  # online connection to receive
+
+        self._socket.bind(self._hostname)
+        self._socket.listen()
+
+        # start connection and build graph process
+        thread1 = threading.Thread(target=self._mk_accept, args=())
+        thread1.start()
+        time.sleep(5)
+        thread2 = threading.Thread(target=self._mk_connection, args=())
+        thread2.start()
+        # end connection and build graph process
+
+    def _mk_accept(self):
+        print(f"[{self._node_name}] Connected, listening on {self._hostname[0]}:{self._hostname[1]}")
+        conn, add = self._socket.accept()
+        self._conn = (conn, add)
+
+        print(f"[{self._node_name}] Successfully all node are connected.")
+
+    def _mk_connection(self):
+        conn = self._connection
+        while True:
+            try:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect(conn)
+                add = sock.getsockname()
+                add = [str(add[0]), str(add[1])]
+                orig = ":".join(add)
+
+                self._socket_conn = (sock, orig)
+                time.sleep(random.randint(5, 9))
+                break
+            except ConnectionRefusedError:
+                continue
--- core/semi_sync/service.py
+++ core/semi_sync/service.py
@@ -12,6 +12,7 @@ from torch import nn
 from core.models.model_factory import create_model
 from core.loader import get_cifar, get_loaders
 from ._node import ServerNode
+from ._node import ClientNode
 
 from settings import DEFAULT_OUTPUT_DIR
 
@@ -47,11 +48,60 @@ class SemiSyncComputationGraphService:
         save_path = DEFAULT_OUTPUT_DIR.joinpath("cluster").joinpath("semi-sync").joinpath(_cu)
         save_path.mkdir(exist_ok=True, parents=True)
 
+        ServerNode.SocketConnections = np.zeros((len(self._selected_clients),))  # initiate the connection
         glob_node_lock = threading.Lock()
 
         server_model = create_model(name=self._model_name, num_classes=self._n_classes, device="cpu")
-        server_node = ServerNode(hostname=self._nodes_conf["server"],
-                                 connections=self._selected_clients,
-                                 model=server_model,
-                                 glob_lock=glob_node_lock,
-                                 save_path=save_path)
+        self._server_node = ServerNode(hostname=self._nodes_conf["server"],
+                                       connections=self._selected_clients,
+                                       model=server_model,
+                                       glob_lock=glob_node_lock,
+                                       save_path=save_path)
+
+        self._nodes = []
+        idx = 0
+        for val in self._selected_clients:
+            (host, port), score = val
+            self._nodes.append(ClientNode(hostname=(host, port),
+                                          connection=self._nodes_conf["server"],
+                                          glob_lock=glob_node_lock,
+                                          host_idx=idx,
+                                          save_path=save_path))
+            idx += 1
+
+        # start, make sure that all computation graph is built
+        while True:
+            glob_node_lock.acquire()
+            if np.all(ServerNode.SocketConnections == 1):
+                glob_node_lock.release()
+                break
+            glob_node_lock.release()
+            time.sleep(3)  # bigger number for bigger computation graph
+        # end, make sure that all computation graph is built
+
+    def train(self, arguments):
+        # start training process
+        print(f"[Train] now start training process on {self._n_classes} nodes")
+
+        start_barrier = threading.Barrier(parties=len(self._nodes)+1)
+        agg_barrier = threading.Barrier(parties=self._nodes_conf["limit"])
+
+        opt_conf = {
+            "lr": arguments.lr,
+            "momentum": arguments.momentum,
+            "weight_decay": arguments.weight_decay
+        }
+
+        train_dataset, test_dataset = get_cifar(self._n_classes)
+        client_loaders, test_loader = get_loaders(train_dataset,
+                                                  test_dataset,
+                                                  n_clients=len(self._nodes),
+                                                  alpha=arguments.alpha,
+                                                  batch_size=arguments.batch_size,
+                                                  n_data=None,
+                                                  num_workers=arguments.n_worker,
+                                                  seed=arguments.seed, )
+
+        for idx, node in enumerate(self._nodes):
+            pass
+
